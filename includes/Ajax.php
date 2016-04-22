0021b80a7ecf331d2ab20c8acc7d97ac0d53a4d5
Bhavin <shah2010bhavin@gmail.com>
2016-04-22 19:58:23 +0530

    Changes after Security Integration

--- includes/Ajax.php
+++ includes/Ajax.php
@@ -5,27 +5,18 @@ add_action('wp_ajax_nopriv_ag_add_device', 'ag_add_device');
 
 // Check if device is added or not , if not then add device.
 
-function ag_add_device()
-{
+function ag_add_device() {
 
     $device_id = filter_input(INPUT_GET, 'device_id', FILTER_SANITIZE_STRING);
 
     $user_id = absint(filter_input(INPUT_GET, 'user_id', FILTER_VALIDATE_INT));
 
-    $token = filter_input(INPUT_GET, 'security_key', FILTER_SANITIZE_STRING);
-
-    var_dump($token);
-
-    var_dump("Hello");
+    $security_key = filter_input(INPUT_GET, 'security_key', FILTER_SANITIZE_STRING);
 
     $res = get_option('ag_settings');
     $get_key = $res['ag_uid'];
 
-    $encryption = md5($device_id . $get_key);
-
-    var_dump($encryption);
-
-
+    $encrypted_key = sha1($device_id . $get_key); // will encrypt key using device_id and unique_id
 
     if (0 == $user_id) { 
         $result = get_option('ag_settings');
@@ -37,20 +28,14 @@ function ag_add_device()
         if (0 < $user_id) {
             $res = get_user_meta($user_id, 'ag_googletoken');
 
-            if (false == in_array($device_id, $res)) {
+            if (false == in_array($device_id, $res) && ($security_key == $encrypted_key)) {
                 add_user_meta($user_id, 'ag_googletoken', $device_id);
 
                 wp_send_json_success();
             }
         }
     }
-
-
-
-
-    wp_send_json_error();
-
-
+        wp_send_json_error();
 
     wp_die(); // this is required to terminate immediately and return a proper response
 }
